420b98d4f7b379a6e5e77f4bce60266e6c90829c
ganavee <itsganavijayaram@gmail.com>
2022-01-30 15:14:30 +0530

    More of String Operation Implementattions

--- String_operations.py
+++ String_operations.py
@@ -35,9 +35,95 @@ def find_substring(s, ss):
     else:
         print("Substring not found")
 
+'''Convert to list'''
+def convert_to_list(s):
+    ch = []
+    for i in (s):
+        ch.append(i)
+    print("The character array is ", ch)
+    convert_to_string(ch)
+    return ch
+
+'''Convert list to string'''
+def convert_to_string(li):
+    s = ""
+    for i in li:
+        s += i
+    print("String is ", s)
+
+
+'''Concatenation of string'''
+def concat(s, s1):
+    for i in s1:
+        s += i
+    print("The new concatenated string is ", s)
+  
+
+'''Slicing'''
+def slicing(s, start=0, end=None):
+    if end is None:
+        end = len(s)
+    ss = ""
+    if(end < 0):
+        end = len(s) + end
+    for i in range(start, end):
+        ss += s[i]
+    print("The sliced string is ", ss)
+
+'''Extended Slicing'''
+def extended_slicing(s, start = None, end =  None, stride = None):
+    ss = ""
+    if(stride >= 0):
+        if start is None:
+            start = 0
+        if end is None:
+            end = len(s)
+        for i in range(start, end, stride):
+            print(i)
+            ss += s[i]
+    else:
+        if start is None:
+            start = len(s)-1
+        if end is None:
+            end = 0
+        for i in range(start, end, stride):
+            ss += s[i]
+    print("New string is ", ss)
+        
+
+'''Replace all  with substring'''
+def replace_substring(s, old_ss, new_ss):
+    length = len(s)- len(old_ss) +1
+    search_start = 0
+    while(search_start < len(s)):
+        count = 0
+        #to find all the substring
+        for j in range(len(old_ss)):
+            if(old_ss[j] != s[search_start+j]):
+                break
+            else:
+                count += 1
+        #To replace old with new substring
+        if(count == len(old_ss)):
+            print("Found at index", search_start)
+            first = s[0:search_start]
+            first += new_ss
+            first += s[search_start+len(old_ss):len(s)]
+            s = first
+            search_start += len(new_ss)
+        else:
+             search_start += 1
+    print("New string ", s)
+            
 
 s = "catdogranpig"
-p = "ran"
+p = "rcranpitran"
 empty = ""
-length(p)
-find_substring(s, '')
+replace_substring(empty, "ran", "porting")
+#extended_slicing(s, start = 12, end = 5, stride = 2)
+#slicing("spam", end = -2)
+#concat(empty, "hello")
+#convert_to_list(s)
+#find_substring(s, '')
+#length(p)
+
