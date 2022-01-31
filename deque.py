7410641f07b01ec23db30d12bc70b8b325ed89e8
ganavee <itsganavijayaram@gmail.com>
2022-01-31 19:28:38 +0530

    Skeleton for deque

---
+++ deque.py
@@ -0,0 +1,32 @@
+#similar to implementation of 2 stacks in an array
+
+class deque():
+    def __init__(self):
+        self.n = int(input("Enter the size of deque"))
+        self.queue = [None] * self.n
+        self.front = -1
+        self.tail = self.n
+
+    def insert_front(self, item)
+        pass
+
+    def insert_rear(self, item):
+        pass
+
+    def delete_front(self):
+        pass
+
+    def delete_rear(self):
+        pass
+
+    def queue_full(self):
+        pass
+
+    def queue_empty(self):
+        pass
+
+    def display(self):
+        pass
+
+obj = deque()
+
