c1aa20ffedf0b875779d7da07a33d6497d6d6679
ganavee <itsganavijayaram@gmail.com>
2022-03-07 10:00:22 +0530

    Search operation added

--- Binary_search_tree_basic_operations.py
+++ Binary_search_tree_basic_operations.py
@@ -25,6 +25,24 @@ class BinarySearchTree:
                 curr.right = Node(data)
             else:
                 self.insert_data(data, curr.right)
+
+    def search_data(self, data, curr):
+        if(curr == None):
+            print("Element not found")
+        elif(data == curr.data):
+            print("Element found")
+        elif(data < curr.data):
+            self.search_data(data, curr.left)
+        elif(data >= curr.data):
+            self.search_data(data, curr.right)
+        else:
+            print("Element not found")
+
+    def search(self, data):
+        if(self.root == None):
+            print("No nodes present hence the element not present")
+            return
+        self.search_data(data, self.root)
         
 
     def display(self):
@@ -44,3 +62,23 @@ obj.insert(6)
 obj.display()
 obj.insert(2)
 obj.display()
+obj.insert(7)
+obj.display()
+obj.insert(5)
+obj.display()
+obj.insert(1)
+obj.display()
+obj.insert(3)
+obj.display()
+obj.insert(4)
+obj.display()
+obj.insert(6)
+obj.display()
+obj.search(1)
+obj.search(2)
+obj.search(3)
+obj.search(4)
+obj.search(5)
+obj.search(6)
+obj.search(7)
+obj.search(8)
