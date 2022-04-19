32b3ac736c549479afc88598a267a1a6f9d4ef9b
ganavee <itsganavijayaram@gmail.com>
2022-04-19 14:33:42 +0530

    Renamed Files

--- Full_binary_tree.py
+++ Full_binary_tree.py
@@ -27,6 +27,37 @@ class Tree:
                     break
                 else:
                     curr = curr.left
+
+    def is_complete_binary_tree(self):
+        level = 0
+        total_level_nodes = 2 ** level
+        node_count = 0
+        curr = self.head
+        queue = [curr]
+        none_nodes = 0
+        flag = False
+        while(len(queue)):
+            curr = queue.pop(0)
+            node_count += 1
+            if(curr != None):
+                queue.append(curr.left)
+                queue.append(curr.right)
+            else:
+                none_nodes += 1
+            if(node_count == total_level_nodes):
+                print("level", level)
+                if(none_nodes != 0):
+                    if(none_nodes != total_level_nodes):
+                        print("The given Tree is not a Complete Binary Tree")
+                        flag = True
+                    break
+                level += 1
+                node_count = 0
+                none_nodes = 0
+                total_level_nodes = 2 ** level
+        if(flag == False):
+            print("The given Tree is a Complete Binary Tree")
+
     def inorder_traversal(self):
         curr = self.head
         stack = []
@@ -43,42 +74,8 @@ class Tree:
                 break
         print("Inorder Traversal is ", inorder)
 
-    def full_binary_tree(self):
-        curr = self.head
-        queue = [curr]
-        flag = False
-        level = 0
-        total_level_nodes = 2**level
-        node_count = 0
-        none_node = 0
-        while(len(queue)):
-            curr = queue.pop(0)
-            node_count += 1
-            if(curr != None):
-                if(flag == True):
-                    print("The given tree is not Full binary tree")
-                    return
-                self.heap.append(curr.data)
-                if(curr.left == None and curr.right != None):
-                    print("The given tree is not Full binary tree")
-                    return
-                queue.append(curr.left)
-                queue.append(curr.right)
-            else:
-                self.heap.append('-')
-                flag = True
-                none_node += 1
-            if(node_count == total_level_nodes):
-                if(none_node == total_level_nodes):
-                    print("The given tree is Full binary tree")
-                    return
-                level += 1
-                total_level_nodes = 2**level
-                node_count = 0
-        
-
 obj = Tree()
-
+'''
 obj.insert(16)
 obj.insert(10)
 obj.insert(21)
@@ -98,6 +95,6 @@ obj.insert(23)
 obj.insert(25)
 obj.insert(24)
 obj.insert(14)
-'''
+
 obj.inorder_traversal()
-obj.full_binary_tree()
+obj.is_complete_binary_tree()
