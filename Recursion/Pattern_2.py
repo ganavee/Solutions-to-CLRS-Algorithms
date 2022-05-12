dbb068cf54f68de276a4e7f1d0b0bdd2da4f94b2
ganavee <itsganavijayaram@gmail.com>
2022-05-12 14:41:47 +0530

    Recursive code for Pattern-2

---
+++ Recursion/Pattern_2.py
@@ -0,0 +1,23 @@
+'''
+Pattern
+*
+* *
+* * *
+* * * *
+'''
+class pattern2:
+    def solve(self, row, column):
+        if(row == 0):
+            return
+        if(column == row):
+            self.solve(row-1, 0)
+            print()
+            pass
+        else:
+            self.solve(row, column+1)
+            print("*", end = " ")
+        
+
+
+obj = pattern2()
+obj.solve(4,0)
