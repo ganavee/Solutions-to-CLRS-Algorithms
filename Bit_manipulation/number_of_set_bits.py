dc074b3d5578792b5713e8dbc331553304796767
ganavee <itsganavijayaram@gmail.com>
2022-05-19 06:59:10 +0530

    Different methods to calculate number of set bits

--- Bit_manipulation/number_of_set_bits.py
+++ Bit_manipulation/number_of_set_bits.py
@@ -11,13 +11,29 @@ class Solution:
                 count += 1
             num >>= 1
         print("Number of set bits in {0} is {1}".format(n, count))
-       
+
+    def method_2(self, num):
+        #find the rightmost set bit(count it) and delete it
+        count = 0
+        while(num):
+            count += 1
+            num = num & (num) - 1
+        print("Number of setbit is ", count)
+            
+
+    def method_3(self, num):
+        count = 0
+        while(num):
+            count += 1
+            num = num - (num & -num)
+        print("Number of setbit is ", count)
     
     def read_input(self):
         print("Enter number")
         num = int(sys.stdin.readline())
         print("Number: {0}".format(num))
-        self.find(num)
+        #self.find(num)
+        self.method_3(num)
 
 obj = Solution()
 obj.read_input()
