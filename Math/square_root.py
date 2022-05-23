1a84b7d9dae200ecd44283938b88abe6ac024eb1
ganavee <itsganavijayaram@gmail.com>
2022-05-23 07:54:22 +0530

    Finding square of a number and rounding it off to particular number of places

--- Math/square_root.py
+++ Math/square_root.py
@@ -33,20 +33,59 @@ class SquareRoot:
             end = mid -1
         elif(mid*mid < n):
             start = mid+1
-        else:
+        elif(mid*mid == n):
             return mid
         return self.find2(start, end, n)
 
     def perfect_sqrt(self, num):
         sqrt = self.find2(1, num, num)
         print("Square root of {0} is {1}".format(num, sqrt))
+
+    def decimal_part(self, incr, rounded, k, n, places, count):
+        #print(" before incr {0} k {1}".format(incr, k))
+        k = round(k, rounded)
+        incr = round(incr, rounded)
+        if(k*k < n):
+            #print("Hello k {0} incr {1}".format(k, incr))
+            return self.decimal_part(incr, rounded, (k+incr), n, places, count)
+        else:
+            if(count == places):
+                return k-incr
+            count += 1
+            return self.decimal_part(incr*0.1, rounded+1, (k-incr)+(incr*0.1), n, places, count)
+
+    def find3(self, start, end, n, places):
+        if(start >= end):
+            if(start*start == n):
+                return start
+            elif(start*start < n):
+                return self.decimal_part(0.1, 1, start+0.1, n, places, 1)
+            else:
+                return self.decimal_part(0.1, 1, start-1+0.1, n, places, 1)
+        mid = (start+end)//2
+        #print("mid ", mid)
+        if(mid*mid > n):
+            end = mid - 1
+        elif(mid*mid < n):
+            start = mid + 1
+        elif(mid*mid == n):
+            return mid
+        return self.find3(start, end, n, places)
+
+    def imperfect_sqrt(self, num, places ):
+        return self.find3(1, num, num, places)
+        pass
     
     def read_input(self):
-        print("Enter Number")
-        num = int(sys.stdin.readline())
-        print("Input Number:", num)
+        print("Enter Number and decimal places")
+        arr = (sys.stdin.readline())
+        arr = list(map(int, arr.strip().split()))
+        num = arr[0]
+        places = arr[1]
+        print("Input Number: {0} decimal places {1}".format(num, places))
         #self.find(num)
-        self.perfect_sqrt(num)
+        integer = self.imperfect_sqrt(num, places)
+        print("Square root of {0} is {1} ".format(num, integer))
 
 obj = SquareRoot()
 obj.read_input()
