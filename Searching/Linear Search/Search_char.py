532526fe607f8f6c47e5ff9409b1d3bc3036d7d4
ganavee <itsganavijayaram@gmail.com>
2022-05-27 11:40:19 +0530

    Linear search in strings

---
+++ Searching/Linear Search/Search_char.py
@@ -0,0 +1,21 @@
+import sys
+class Selection_sort:
+    def search(self, string, char):
+        for i in string:
+            if(i == char):
+                return True
+        return False
+        pass
+    
+    def input(self):
+        print("Enter the String")
+        string = sys.stdin.readline().strip()
+        print("Enter character to be searched")
+        char = sys.stdin.readline().strip()
+        print("Input String = {0} and character is {1}".format(string, char))
+        print("Is {0} present in {1} ? {2}".format(char, string, self.search(string, char)))
+        
+
+
+obj = Selection_sort()
+obj.input()
