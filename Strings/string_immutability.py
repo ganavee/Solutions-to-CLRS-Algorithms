fc606927868159d27c062209ba55b6192aadb13e
ganavee <itsganavijayaram@gmail.com>
2022-06-27 11:50:23 +0530

    Strings are immutable

---
+++ Strings/string_immutability.py
@@ -0,0 +1,27 @@
+#Same objects have same id 
+s = "Ganavi"
+p = "Ganavi"
+q = "Jayaram"
+print("id of s = {0} id of p = {1} id of q = {2}".format(id(s), id(p), id(q)))
+
+#To create different objects with same value
+a = "Ganavi"
+b = a.encode().decode()
+print("id of a = {0} id of b = {1}".format(id(a), id(b)))
+
+#Strings immutablity
+
+#Here different objects called Ganavi and Hello are created. so we are not changing any string
+c = "Ganavi"
+d = c
+print("Before modification id of c = {0} id of d = {1}".format(id(c), id(d)))
+d = "Hello"
+print("After modification id of c = {0} id of d = {1}".format(id(c), id(d)))
+print("d = ", d)
+
+'''
+#Strings are immutable. Here we are changing the strings itself and hence not possible
+e = "Ganavi"
+e[0] = 'F'
+print(e)
+'''
