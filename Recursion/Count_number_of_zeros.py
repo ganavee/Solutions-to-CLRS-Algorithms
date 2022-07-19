3f35778108a0fe0d290197c76bc7095b3ec6f742
ganavee <itsganavijayaram@gmail.com>
2022-07-19 06:47:52 +0530

    Adding Tail recursion optimisation to counting number of 0's

--- Recursion/Count_number_of_zeros.py
+++ Recursion/Count_number_of_zeros.py
@@ -1,10 +1,25 @@
-def count(n, tot):
-    if(n == 0):
-        return tot
-    if(n%10 == 0):
-        tot += 1
-    return count(n//10, tot)
-    pass
+import sys
 
-num = 2050060
-print("Number of zeros in {0} is {1}".format(num, count(num, 0)))
+class Recursion:
+    def tail_recursion(self, n, a):
+        if(n == 0):
+            return a
+        if(n%10 == 0):
+            return (self.tail_recursion(n//10, a+1))
+        else:
+            return(self.tail_recursion(n//10,a))
+
+    def non_tail_recursion(self, n):
+        if(n == 0):
+            return 0
+        if(n%10 == 0):
+            return 1 + self.non_tail_recursion(n//10)
+        else:
+            return 0 + self.non_tail_recursion(n//10)
+
+
+obj = Recursion()
+print("Enter a number")
+input = int(sys.stdin.readline())
+print("Number of 0's in {0} is {1} using Non Tail Recursion".format(input, obj.non_tail_recursion(input)))
+print("Number of 0's in {0} is {1} using Tail Recursion".format(input, obj.tail_recursion(input, 0)))
