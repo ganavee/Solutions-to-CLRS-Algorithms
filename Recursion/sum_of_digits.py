3f1b8d0dbdfb09fcab160d611cd6478c1e3e304a
ganavee <itsganavijayaram@gmail.com>
2022-07-19 06:31:08 +0530

    Added tail recursion optimisation

--- Recursion/sum_of_digits.py
+++ Recursion/sum_of_digits.py
@@ -1,7 +1,19 @@
-def sum_of_digits(n):
-    if(n == 0):
-        return 0
-    return (n%10) + sum_of_digits(n//10)
+import sys
 
-num = 1342
-print("Sum of the digits of num {0} is {1}".format(num, sum_of_digits(num)))
+class Recursion:
+    def tail_recursion(self, n, a):
+        if(n == 0):
+            return a
+        return (self.tail_recursion(n//10, (n%10)+a))
+
+    def non_tail_recursion(self, n):
+        if(n == 0):
+            return 0
+        return (n%10) + self.non_tail_recursion(n//10)
+
+
+obj = Recursion()
+print("Enter a number")
+input = int(sys.stdin.readline())
+print("Sum of digits {0} is {1} using Non Tail Recursion".format(input, obj.non_tail_recursion(input)))
+print("Sum of digits {0} is {1} using Tail Recursion".format(input, obj.tail_recursion(input, 0)))
