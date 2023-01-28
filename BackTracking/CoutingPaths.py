c38e68c4fd9661a518e88dd6f733f372e5db0d2d
ganavijayaram <jayaram.g@northeastern.edu>
2023-01-28 12:33:18 -0500

    Printing path

--- BackTracking/CoutingPaths.py
+++ BackTracking/CoutingPaths.py
@@ -11,10 +11,22 @@ def countPath(mat, src, des):
     pass
 
 
+def path(mat, src, des, org, p):
+    if(src[0] == des[0] and src[1] == des[1]):
+        print("Path: ", p)
+        return p
+    if(src[0] < des[0]):
+        downPath = path(mat, [src[0]+1, src[1]], des, False, p+"D")
+    if(src[1] < des[1]):
+        rightPath = path(mat, [src[0], src[1]+1], des, False, p + "R")
+    
+    
+
 mat = [3,3]
 src = [0, 0]
 des = [2,2]
 if(des[0] < mat[0] and des[0] >= 0 and des[1] < mat[1] and des[1] >= 0):
     print("Ways: ", countPath(mat, src, des))
+    path(mat, src, des, True, "")
 else:
     print("Please enter valid Source and Destination")
