1add792ced3ed72963e1d1379f80c8748f3bc293
ganavijayaram <jayaram.g@northeastern.edu>
2023-05-14 06:23:13 -0400

    Subsequence whose sum is k

---
+++ Recursion/subsequenceWhoseSumIsK.py
@@ -0,0 +1,41 @@
+import copy 
+#Printing the subsequnce whose sum is k
+def newSeq(subSum, subTup, tup, k, arr, i):
+    #print(f"subSum = {subSum} subTup = {subTup} tup = {tup}, arr = {arr}")
+    if(len(tup) == 0):
+        if(subSum == k):
+            arr.append(subTup)
+        return arr
+    ele = tup[0]
+    newTup = tup[1:]
+    del tup
+    #print(f"First{i}: subSum = {subSum} subTup = {subTup} tup = {tup}, arr = {arr}")
+    newSeq(subSum, subTup, newTup, k, arr, i+1)
+    newSubTup = subTup + (ele, )
+    del subTup
+    #print(f"Second{i}: subSum = {subSum} subTup = {subTup} tup = {tup}, arr = {arr}")
+    newSeq(subSum+ele, newSubTup, newTup, k, arr, i+1)
+    return arr  
+
+#Works for single digit number
+def subseq1(sumString, string, seq, arr, k):
+    if(seq == ""):
+        if(sumString == k):
+            arr.append(string)
+        return arr
+    popChar = seq[-1]
+    popNum = int(popChar)
+    seq = seq[:-1]
+    subseq1(sumString, string, seq, arr, k)
+    subseq1(popNum + sumString, popChar + string, seq, arr, k)
+    return arr
+
+print ("\n" * 100)
+seq = [int(i) for i in input("Enter the subsequence: ").split(",")]
+k = int(input("Enter the value of k: "))
+arr = []
+seq = tuple(seq)
+print(f"Subsequence of {seq} whose sum is equal to {k} is {newSeq(0, (), seq, k, arr, 1)}")
+#stringSeq = input("Enter the String subsequence: ")
+#string = ""
+#print(f"Subsequences of {seq} is {subseq1(0, string, stringSeq, arr, k)}")
