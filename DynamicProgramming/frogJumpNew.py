a240407fb1bf9c8d26293816e906a267dadba9be
ganavijayaram <jayaram.g@northeastern.edu>
2023-05-17 07:27:06 -0400

    Frog jump using recursio

--- DynamicProgramming/frogJumpNew.py
+++ DynamicProgramming/frogJumpNew.py
@@ -5,7 +5,7 @@ from math import *
 
 from typing import *
 
-
+#Memoization
 def frog(arr, n, dp):
     if(n == 0):
         return 0
@@ -25,3 +25,25 @@ def frogJump(n: int, heights: List[int]) -> int:
     dp = [-1 for i in range(n)]
     return frog(heights, n-1, dp)
     
+#Recursive solution
+def rec(currStep, n, heights, minEnergy, currEnergy):
+    if(currStep == n-1):
+        if(minEnergy > currEnergy):
+            minEnergy = currEnergy
+        return minEnergy
+    if(currStep > n):
+        return minEnergy
+    minEnergy = rec(currStep+1, n, heights, minEnergy,
+                 currEnergy+abs(heights[currStep] - heights[currStep+1]))
+    if(currStep+2 < n):
+        minEnergy = rec(currStep+2, n, heights, minEnergy, 
+                 currEnergy+abs(heights[currStep] - heights[currStep+2]))
+    return minEnergy
+
+
+def frogJump1(n, heights):
+    #dp = [-1 for i in range(n+1)]
+    print(f"Solution1: {rec(0, n, heights, maxsize, 0)}")
+
+height = [7, 4, 4, 2, 6, 6, 3, 4] 
+print(f"Solution2: {frogJump(len(height), height)}")
